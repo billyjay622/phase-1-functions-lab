e3de40431f91da9bf99d9ec8c3c939487d237197
William Barr <williamjaybarr@gmail.com>
2022-07-18 20:46:23 -0400

    commit tree 760d76cbe1b5d89a3f7f47598f50003f988f27b6

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+   if (pickupLocation > 42) {
+    return (pickupLocation - 42)}
+    else {
+    return 42 - pickupLocation
+    }
+  }
+function distanceFromHqInFeet(pickupLocation) {
+   return distanceFromHqInBlocks(pickupLocation) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+    return ((destination - start) * 264)}
+    else {
+    return ((start - destination) * 264)
+    }
+  }
+  function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0;
+      } else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return ((distanceTravelledInFeet(start, destination) - 400) * .02);
+      } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+      } else {
+        return 'cannot travel that far';
+      }
+  }
